c7d21149506ea6db759eb4fc3f5b503eab3d9e35
oleksii <petrlesha@gmail.com>
2024-10-25 17:43:38 +0200

    postcss + tailwind connection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,68 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import 'carbon-components-svelte/css/all.css';
+	import '$lib/_global.css';
+
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+		HeaderNavMenu,
+		SideNav,
+		SideNavItems,
+		SideNavMenu,
+		SideNavMenuItem,
+		SideNavLink,
+		SideNavDivider,
+		SkipToContent,
+		Content,
+	} from 'carbon-components-svelte';
+
+	let isSideNavOpen = false;
 
 	let ready: boolean = false;
 	onMount(() => (ready = true));
 </script>
 
-<div class="dragbar" />
+<Header
+	platformName="Technical Skills in Beach Volleyball"
+	bind:isSideNavOpen
+	class="w-full"
+	style="-webkit-app-region: drag;"
+>
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+	<HeaderNav style="-webkit-app-region: drag;">
+		<HeaderNavItem href="/" text="Link 1" />
+		<HeaderNavItem href="/" text="Link 2" />
+		<HeaderNavItem href="/" text="Link 3" />
+		<HeaderNavMenu text="Menu">
+			<HeaderNavItem href="/" text="Link 1" />
+			<HeaderNavItem href="/" text="Link 2" />
+			<HeaderNavItem href="/" text="Link 3" />
+		</HeaderNavMenu>
+		<HeaderNavItem href="/" text="Link 4" />
+	</HeaderNav>
+</Header>
 
-{#if ready}
-	<slot />
-{/if}
+<SideNav bind:isOpen={isSideNavOpen}>
+	<SideNavItems>
+		<SideNavLink text="Link 1" />
+		<SideNavLink text="Link 2" />
+		<SideNavLink text="Link 3" />
+		<SideNavMenu text="Menu">
+			<SideNavMenuItem href="/" text="Link 1" />
+			<SideNavMenuItem href="/" text="Link 2" />
+			<SideNavMenuItem href="/" text="Link 3" />
+		</SideNavMenu>
+		<SideNavDivider />
+		<SideNavLink text="Link 4" />
+	</SideNavItems>
+</SideNav>
 
-<style>
-	.dragbar {
-		-webkit-app-region: drag;
-		position: absolute;
-		z-index: 100;
-		height: 40px;
-		width: 100%;
-	}
-</style>
+<Content>
+	{#if ready}
+		<slot />
+	{/if}
+</Content>
