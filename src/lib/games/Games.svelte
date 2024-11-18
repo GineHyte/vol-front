856e2a840bc83d34fd26a688020d8db603dba81b
minebyte <petrlesha@gmail.com>
2024-11-18 22:26:06 +0100

    WIP

--- src/lib/games/Games.svelte
+++ src/lib/games/Games.svelte
@@ -1,10 +1,11 @@
 <script>
 	import Field from './Field.svelte';
 	import SideList from '$lib/ui/SideList.svelte';
-	import { Players } from '$lib/scripts/players';
+	import { Games } from '$lib/scripts/games';
 </script>
 
 <div class="flex items-center w-full justify-center p-4">
 	<Field />
 </div>
-<SideList model={new Players()} headers={[{ first_name: 'First Name' }]} />
+
+<SideList model={new Games()} headers={[{ key: 'name', value: 'Games' }]} />
