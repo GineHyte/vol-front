117d174a10474f05621b5aa7c5580a2f4b5c1f99
GineHyte <petrlesha@gmail.com>
2025-01-15 20:35:18 +0200

    WIP

--- src/electron.cjs
+++ src/electron.cjs
@@ -70,11 +70,11 @@ contextMenu({
   ],
 });
 
-function loadVite(port) {
+function loadDev(port) {
   mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
     console.log('Error loading URL, retrying', e);
     setTimeout(() => {
-      loadVite(port);
+      loadDev(port);
     }, 200);
   });
 }
@@ -94,7 +94,7 @@ function createMainWindow() {
     mainWindow = null;
   });
 
- loadProduction();
+ isDev ? loadDev(port) : loadProduction();
 }
 
 app.once('ready', createMainWindow);
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'path';
 
 const config = {
-  resolve: {
-    alias: {
-      "": path.join(__dirname, 'src/app')
-    },
-  },
   plugins: [sveltekit()],
 };
 
